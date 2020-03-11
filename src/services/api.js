9bb7c1796b8404edf8a6c80bd72edc930aa018f2
Raphael Buriti <raphael_buriti@outlook.com>
2020-03-10 23:04:10 -0300

    wip

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const api = axios.create({
 
     baseURL: process.env.REACT_APP_API_URL
+    // baseURL: 'https://moretimefree-backend.herokuapp.com/'
     // baseURL: 'http://189.100.143.108:9999/'
     // baseURL: 'http://localhost:9997/'
     // baseURL: 'https://morning-crag-27342.herokuapp.com/'
