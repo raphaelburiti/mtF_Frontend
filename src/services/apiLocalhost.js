f8abeea74b60c440abdfd0af3689a824328b2496
Raphael Buriti <raphael_buriti@outlook.com>
2020-03-13 13:16:32 -0300

    wip

--- src/services/apiLocalhost.js
+++ src/services/apiLocalhost.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiLocalhost = axios.create({
     // baseURL: 'http://localhost:9999/'
-    baseURL: 'http://appselenium.ddns.net:9696/'
+    baseURL: process.env.REACT_APP_SELENIUM_URL
 })
 
 export default apiLocalhost;
